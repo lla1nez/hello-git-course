d1c5eb06184d05dee47051fb0f1d91c3bd829725
Lauri Laine <lauri.m.laine@tuni.fi>
2022-12-21 21:11:50 +0200

    korjattu

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,1 @@
-"""
-Ohjelmointi 1: Tervehdys
-Nimi: Lauri Laine
-Opiskelijanumero: 150485944
-"""
-nimi = input("Tell your name: ")
-print("Hi", nimi)
-print("your coding skills are great!")
+print("Hello World!")
